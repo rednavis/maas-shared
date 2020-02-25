779571f9b8e66020f7f67ad99e6f553c2f93e2be
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2020-02-25 17:09:21 +0300

    Small refactoring

---
+++ src/main/java/com/rednavis/shared/RestUrl.java
@@ -0,0 +1,20 @@
+package com.rednavis.shared;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class RestUrl {
+
+  public static final String AUTH_URL = "/api/auth";
+  public static final String AUTH_URL_PATTERN = AUTH_URL + "/**";
+  public static final String AUTH_URL_SIGNIN = "/signin";
+  public static final String AUTH_URL_SIGNUP = "/signup";
+  public static final String AUTH_URL_TEST_POST = "/testPost";
+  public static final String AUTH_URL_TEST_GET = "/testGet";
+
+  public static final String USER_URL = "/api/users";
+  public static final String USER_URL_PATTERN = USER_URL + "/**";
+  public static final String USER_URL_ME = "/me";
+  public static final String USER_URL_USER = "/user";
+  public static final String USER_URL_ADMIN = "/admin";
+}
---
+++ src/main/java/com/rednavis/shared/dto/auth/TestRequest.java
@@ -0,0 +1,11 @@
+package com.rednavis.shared.dto.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TestRequest {
+
+  private String valueInput;
+}
