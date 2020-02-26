eaac1beb8036604db8e2ca71260be09ad91213ae
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2020-02-26 10:19:16 +0300

    Update shared DTO

--- src/main/java/com/rednavis/shared/dto/auth/SignInResponse.java
+++ src/main/java/com/rednavis/shared/dto/auth/SignInResponse.java
@@ -1,10 +1,17 @@
 package com.rednavis.shared.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignInResponse {
 
+  @Builder.Default
   private String tokenType = "Bearer";
   private String accessToken;
 }
--- src/main/java/com/rednavis/shared/dto/user/User.java
+++ src/main/java/com/rednavis/shared/dto/user/User.java
@@ -1,9 +1,15 @@
 package com.rednavis.shared.dto.user;
 
 import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
   private String id;
