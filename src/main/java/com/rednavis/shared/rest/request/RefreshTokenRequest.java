648df8297ddc35d1c17b317ba7b5e05919ceeec1
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2020-03-01 16:50:12 +0300

    Add support Refresh JWT Token

---
+++ src/main/java/com/rednavis/shared/rest/request/RefreshTokenRequest.java
@@ -0,0 +1,15 @@
+package com.rednavis.shared.rest.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenRequest {
+
+  private String refreshToken;
+}
--- src/main/java/com/rednavis/shared/util/RestUrlUtils.java
+++ src/main/java/com/rednavis/shared/util/RestUrlUtils.java
@@ -10,8 +10,7 @@ public class RestUrlUtils {
   public static final String AUTH_URL_CURRENTUSER = "/currentUser";
   public static final String AUTH_URL_SIGNIN = "/signin";
   public static final String AUTH_URL_SIGNUP = "/signup";
-  public static final String AUTH_URL_TEST_POST = "/testPost";
-  public static final String AUTH_URL_TEST_GET = "/testGet";
+  public static final String AUTH_URL_REFRESH_TOKEN = "/refreshToken";
 
   public static final String USER_URL = "/api/users";
   public static final String USER_URL_PATTERN = USER_URL + "/**";
