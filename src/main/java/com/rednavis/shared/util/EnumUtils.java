76db3483470ea8f1f9dcf1bc79a0e5c02d860feb
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2020-03-04 16:09:10 +0300

    Design LoginView

---
+++ src/main/java/com/rednavis/shared/util/EnumUtils.java
@@ -0,0 +1,12 @@
+package com.rednavis.shared.util;
+
+import java.util.Arrays;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class EnumUtils {
+
+  public static String[] getNames(Class<? extends Enum<?>> e) {
+    return Arrays.stream(e.getEnumConstants()).map(Enum::name).toArray(String[]::new);
+  }
+}
