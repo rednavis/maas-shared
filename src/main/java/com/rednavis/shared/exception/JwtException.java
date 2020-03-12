c9140a968613943ea6d358a24db30ebf47533cee
Aliaksei Labotski <aliaksei.labotski@gmail.com>
2020-03-12 05:42:52 +0300

    Update global exception handler

--- src/main/java/com/rednavis/shared/exception/JwtException.java
+++
@@ -1,9 +0,0 @@
-package com.rednavis.shared.exception;
-
-public class JwtException extends RuntimeException {
-
-  public JwtException(String message) {
-    super(message);
-  }
-
-}
--- src/main/java/com/rednavis/shared/rest/response/ErrorResponse.java
+++ src/main/java/com/rednavis/shared/rest/response/ErrorResponse.java
@@ -1,12 +1,23 @@
 package com.rednavis.shared.rest.response;
 
+import java.util.Date;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
 
-@Getter
+@Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
+@FieldNameConstants
 public class ErrorResponse {
 
-  private int code;
+  private Date timestamp;
+  private String path;
+  private String exceptionId;
+  private String error;
   private String message;
+  private Integer status; // We'd need it as integer in JSON serialization
 }
